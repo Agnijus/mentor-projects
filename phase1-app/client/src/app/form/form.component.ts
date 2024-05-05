0edc2699617fa223713fc2db692bfac39ce657fb
Agnijus <agnijus.botyrius01@gmail.com>
2024-05-06 00:30:08 +0100

    feat(client): added loading before notification & reset response after request

--- phase1-app/client/src/app/form/form.component.ts
+++ phase1-app/client/src/app/form/form.component.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { TransactionService } from '../_services/transaction.service';
 import { CommonModule } from '@angular/common';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @Component({
   selector: 'app-form',
   standalone: true,
@@ -15,6 +17,7 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     MatButtonModule,
     CommonModule,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
@@ -23,24 +26,33 @@ export class FormComponent {
   inputText: string = '';
   notification: string | null = null;
   response: Response | null = null;
+  isLoading: boolean = false;
 
   constructor(private transactionService: TransactionService) {}
 
   onSubmit() {
     if (this.inputText) {
+      this.response = null;
+      this.isLoading = true;
+
       this.transactionService.sendTransaction(this.inputText).subscribe({
         next: (res: Response) => {
           console.log(res);
+          this.isLoading = false;
+          this.resetForm();
           this.response = res;
           this.notification = 'The message has been saved';
           setTimeout(() => (this.notification = null), 3000);
-          this.resetForm();
         },
         error: (error) => {
           console.log(error);
+          this.isLoading = false;
           this.notification = 'Failed to save the message';
           setTimeout(() => (this.notification = null), 3000);
         },
+        // complete: () => {
+        //   this.isLoading = false;
+        // },
       });
     }
   }
